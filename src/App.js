abb2a8ed64b2796886a45bd0bdc87eb3032600c0
Nathan Arritt <nathan@arritt.com>
2020-05-04 11:55:13 -0700

    Make state and stagesNames class fields instead of adding in the constructor

--- src/App.js
+++ src/App.js
@@ -10,25 +10,23 @@ import { DIRECTION } from './constants';
 const NUM_STAGES = 4;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [
-          { name: 'task 0', stage: 0 },
-          { name: 'task 1', stage: 0 },
-          { name: 'task 2', stage: 0 },
-          { name: 'task 3', stage: 0 },
-          { name: 'task 4', stage: 1 },
-          { name: 'task 5', stage: 1 },
-          { name: 'task 6', stage: 1 },
-          { name: 'task 7', stage: 2 },
-          { name: 'task 8', stage: 2 },
-          { name: 'task 9', stage: 3 },
-      ],
-      selectedTaskName: '',
-    };
-    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
-  }
+  state = {
+    tasks: [
+        { name: 'task 0', stage: 0 },
+        { name: 'task 1', stage: 0 },
+        { name: 'task 2', stage: 0 },
+        { name: 'task 3', stage: 0 },
+        { name: 'task 4', stage: 1 },
+        { name: 'task 5', stage: 1 },
+        { name: 'task 6', stage: 1 },
+        { name: 'task 7', stage: 2 },
+        { name: 'task 8', stage: 2 },
+        { name: 'task 9', stage: 3 },
+    ],
+    selectedTaskName: '',
+  };
+
+  stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
 
   handleMoveTask = direction => {
     const { tasks, selectedTaskName } = this.state;
