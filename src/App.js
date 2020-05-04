9fb0a45351d2434ba199e90ee6dc0c2472f4b341
Nathan Arritt <nathan@arritt.com>
2020-05-03 19:41:34 -0700

    add project files

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Controls from './components/Controls';
+import Board from './components/Board';
+
+const NUM_STAGES = 4;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+          { name: 'task 0', stage: 0, selected: false },
+          { name: 'task 1', stage: 0, selected: false },
+          { name: 'task 2', stage: 0, selected: false },
+          { name: 'task 3', stage: 0, selected: false },
+          { name: 'task 4', stage: 1, selected: false },
+          { name: 'task 5', stage: 1, selected: false },
+          { name: 'task 6', stage: 1, selected: false },
+          { name: 'task 7', stage: 2, selected: false },
+          { name: 'task 8', stage: 2, selected: false },
+          { name: 'task 9', stage: 3, selected: false },
+      ],
+    };
+    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+  }
+
+  handleMoveTask = (selectedTaskName, direction) => {
+    const { tasks } = this.state;
+    const nextTasks = [...tasks];
+    const selectedTaskIndex = tasks.findIndex(task => task.name === selectedTaskName);
+    const selectedTask = { ...nextTasks[selectedTaskIndex] };
+    if (direction === 'back' && selectedTask.stage > 0) {
+      selectedTask.stage -= 1;
+    } else if (direction === 'forward' && selectedTask.stage < 3) {
+      selectedTask.stage += 1;
+    }
+    nextTasks.splice(selectedTaskIndex, 1);
+    nextTasks.push(selectedTask);
+    this.setState({ tasks: nextTasks });
+  };
+
+  handleSelectTask = (selectedTaskName) => {
+    const { tasks } = this.state;
+    const nextTasks = [...tasks].map(task => {
+      if (task.name === selectedTaskName) {
+        task.selected = true;
+      } else {
+        task.selected = false;
+      }
+      return task;
+    });
+    this.setState({ tasks: nextTasks });
+  };
+
+  render() {
+    const { tasks } = this.state;
+
+    let stagesTasks = [];
+    let selectedTask;
+    for (let i = 0; i < NUM_STAGES; ++i) {
+      stagesTasks.push([]);
+    }
+    for (let task of tasks) {
+      const stageId = task.stage;
+      stagesTasks[stageId].push(task);
+
+      if (task.selected) {
+        selectedTask = task;
+      }
+    }
+
+    return (
+      <div className="App">
+        <Controls
+          handleMoveTask={this.handleMoveTask}
+          selectedTask={selectedTask}
+        />
+        <Board
+          stagesTasks={stagesTasks}
+          stagesNames={this.stagesNames}
+          handleSelectTask={this.handleSelectTask}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Controls.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Controls extends Component {
+  handleBack = () => {
+    const { handleMoveTask, selectedTask } = this.props;
+    handleMoveTask(selectedTask.name, 'back');
+  };
+
+  handleForward = () => {
+    const { handleMoveTask, selectedTask } = this.props;
+    handleMoveTask(selectedTask.name, 'forward');
+  };
+
+  render() {
+    const { handleMoveBack, handleMoveForward, selectedTask } = this.props;
+
+    return (
+      <div style={{ padding: '1rem', background: '#D6F3FF' }}>
+        <h1>Controls</h1>
+        <div style={{ display: 'flex', marginTop: '1rem' }}>
+          <input
+            readOnly
+            placeholder="Selected task name"
+            style={{ fontSize: '1rem' }}
+            data-testid="selected-task-field"
+            value={selectedTask ? selectedTask.name : ''}
+          />
+          <button
+            style={{ marginLeft: '1rem' }}
+            disabled={!selectedTask || selectedTask.stage === 0}
+            data-testid="move-back-btn"
+            onClick={this.handleBack}
+          >
+            Move back
+          </button>
+          <button
+            style={{ marginLeft: '1rem' }}
+            disabled={!selectedTask || selectedTask.stage === 3}
+            data-testid="move-forward-btn"
+            onClick={this.handleForward}
+          >
+            Move forward
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Controls;
