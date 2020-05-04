c478c8a848d4101c8cac1225e8bfe85eb5b7013e
Nathan Arritt <nathan@arritt.com>
2020-05-03 21:10:26 -0700

    Improve selected task and Controls designs

--- src/App.js
+++ src/App.js
@@ -11,25 +11,26 @@ class App extends Component {
     super(props);
     this.state = {
       tasks: [
-          { name: 'task 0', stage: 0, selected: false },
-          { name: 'task 1', stage: 0, selected: false },
-          { name: 'task 2', stage: 0, selected: false },
-          { name: 'task 3', stage: 0, selected: false },
-          { name: 'task 4', stage: 1, selected: false },
-          { name: 'task 5', stage: 1, selected: false },
-          { name: 'task 6', stage: 1, selected: false },
-          { name: 'task 7', stage: 2, selected: false },
-          { name: 'task 8', stage: 2, selected: false },
-          { name: 'task 9', stage: 3, selected: false },
+          { name: 'task 0', stage: 0 },
+          { name: 'task 1', stage: 0 },
+          { name: 'task 2', stage: 0 },
+          { name: 'task 3', stage: 0 },
+          { name: 'task 4', stage: 1 },
+          { name: 'task 5', stage: 1 },
+          { name: 'task 6', stage: 1 },
+          { name: 'task 7', stage: 2 },
+          { name: 'task 8', stage: 2 },
+          { name: 'task 9', stage: 3 },
       ],
+      selectedTaskName: '',
     };
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
   }
 
   handleMoveTask = (direction) => {
-    const { tasks } = this.state;
+    const { tasks, selectedTaskName } = this.state;
     const nextTasks = [...tasks];
-    const selectedTaskIndex = tasks.findIndex(task => task.selected);
+    const selectedTaskIndex = nextTasks.findIndex(task => task.name === selectedTaskName);
     const selectedTask = { ...nextTasks[selectedTaskIndex] };
     if (direction === 'back' && selectedTask.stage > 0) {
       selectedTask.stage -= 1;
@@ -42,20 +43,11 @@ class App extends Component {
   };
 
   handleSelectTask = (selectedTaskName) => {
-    const { tasks } = this.state;
-    const nextTasks = tasks.map(task => {
-      if (task.name === selectedTaskName) {
-        task.selected = true;
-      } else {
-        task.selected = false;
-      }
-      return task;
-    });
-    this.setState({ tasks: nextTasks });
+    this.setState({ selectedTaskName });
   };
 
   render() {
-    const { tasks } = this.state;
+    const { tasks, selectedTaskName } = this.state;
 
     let stagesTasks = [];
     let selectedTask;
@@ -66,7 +58,7 @@ class App extends Component {
       const stageId = task.stage;
       stagesTasks[stageId].push(task);
 
-      if (task.selected) {
+      if (task.name === selectedTaskName) {
         selectedTask = task;
       }
     }
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Controls extends Component {
-  handleBack = () => {
-    const { handleMoveTask } = this.props;
+const Controls = ({ handleMoveTask, selectedTask }) => {
+  const handleBack = () => {
     handleMoveTask('back');
   };
 
-  handleForward = () => {
-    const { handleMoveTask } = this.props;
+  const handleForward = () => {
     handleMoveTask('forward');
   };
 
-  render() {
-    const { selectedTask } = this.props;
-
-    return (
-      <div style={{ padding: '1rem', background: '#D6F3FF' }}>
-        <h1>Controls</h1>
-        <div style={{ display: 'flex', marginTop: '1rem' }}>
-          <input
-            readOnly
-            placeholder="Selected task name"
-            style={{ fontSize: '1rem' }}
-            data-testid="selected-task-field"
-            value={selectedTask ? selectedTask.name : ''}
-          />
-          <button
-            style={{ marginLeft: '1rem' }}
-            disabled={!selectedTask || selectedTask.stage === 0}
-            data-testid="move-back-btn"
-            onClick={this.handleBack}
-          >
-            Move back
-          </button>
-          <button
-            style={{ marginLeft: '1rem' }}
-            disabled={!selectedTask || selectedTask.stage === 3}
-            data-testid="move-forward-btn"
-            onClick={this.handleForward}
-          >
-            Move forward
-          </button>
-        </div>
+  return (
+    <div style={{ padding: '1rem', background: '#D6F3FF' }}>
+      <h1>Controls</h1>
+      <div style={{ display: 'flex', marginTop: '1rem' }}>
+        <input
+          readOnly
+          placeholder="Selected task name"
+          style={{ fontSize: '1rem' }}
+          data-testid="selected-task-field"
+          value={selectedTask ? selectedTask.name : ''}
+        />
+        <button
+          style={{ marginLeft: '1rem' }}
+          disabled={!selectedTask || selectedTask.stage === 0}
+          data-testid="move-back-btn"
+          onClick={handleBack}
+        >
+          Move back
+        </button>
+        <button
+          style={{ marginLeft: '1rem' }}
+          disabled={!selectedTask || selectedTask.stage === 3}
+          data-testid="move-forward-btn"
+          onClick={handleForward}
+        >
+          Move forward
+        </button>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
 export default Controls;
